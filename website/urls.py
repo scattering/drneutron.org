b0d082d5fb4016f27fa48b5ab72f42efecbcfc82
Paul Kienzle <pkienzle@nist.gov>
2014-03-04 15:36:07 -0500

    url fix

--- website/urls.py
+++ website/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 urlpatterns = patterns(
     'website.views',
-    r'^accounts/', include('userena.urls'),
-    r'^admin/doc/', include('django.contrib.admindocs.urls'),
-    r'^admin/', include('django.contrib.admin.site.urls'),
+    (r'^accounts/', include('userena.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 
     # welcome page for launching apps
     url('^$', 'home'),
