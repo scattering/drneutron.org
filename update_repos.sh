7e138ad1d41ae29e24e828889adb9f39fbc81f4d
Paul Kienzle <pkienzle@nist.gov>
2014-03-04 13:32:29 -0500

    improve update script

--- update_repos.sh
+++ update_repos.sh
@@ -10,31 +10,74 @@
 #
 #     git, hg, latex (for docs)
 #
-# Python should be running in a virtual environment with the following
-# packages installed:
+# Python should be running in a virtual environment.  If it is a conda
+# environment, then conda can install all the required tools, but if it
+# is a virtualenv environment, then packages not easily installed with
+# pip need to already be present.  Use the following to see which:
 #
-#     pip, setuptools
-#     numpy, scipy, h5py
-#     matplotlib (for gridding?)
+#     grep ^requires update_repos.sh
 #
 # Update SECTION 1 with any new dependencies.  You may need to reset
 # the virtual env, or explicitly pull new versions as dependencies change.
 #
-#
 
+type conda >/dev/null 2>&1 && use_conda=1
+test $use_conda && install="conda_install" || install="pip install"
 
 # === SECTION 1 ===
 # dependency configuration
 
-# installable dependencies
-requires="setuptools numpy scipy h5py matplotlib"
-installs="nose sphinx coverage rjsmin rcssmin"
-djinstalls="userena apptemplates"
+# "requires" are packages that are difficult to install with pip, e.g.,
+#   because the require preinstalled system libraries, or otherwise have a
+#   large binary component.  These can all be installed directly by conda.
+# "installs" are the names of the python import module used to check if the
+#   package is installed.  If the package install name differs from the
+#   python module name, then define "package_IMPORTNAME=INSTALLNAME".
+# "django_installs" are packages whose install name is "django-NAME"
+
+requires="pip setuptools scipy numpy h5py matplotlib redis IPython"
+installs="nose sphinx coverage rjsmin rcssmin PIL south django"
+django_installs="userena apptemplates"
+
+installs="$installs $django_installs"
+
+# Packages whose install name differs from the import name
+for p in $django_installs; do declare package_$p=django-$p; done
+package_PIL=pillow
+package_redis=redis-py
+package_IPython=ipython
 
 # === SECTION 2 ===
 # library
 
 prefix="=== "
+
+conda_install ()
+{
+   echo "Trying to install $* with conda..."
+   conda install --yes $* || pip install $*
+}
+
+# Retrieve package name for a given python module
+# usage: package module
+#
+# Usually the package name matches the python module name, but in cases like
+# IPython, the package name is instead "ipython".  In those cases where they
+# differ, set package_module=package.  If you have an entire list of modules
+# with the same pattern, e.g., django-module, you can set a variable to a list
+# of modules and use:
+#
+#      for p in $mods; do declare package_$p=django-$p; done
+#
+package ()
+{
+    varname=package_$1
+    test ${!varname} && echo ${!varname} || echo $1
+}
+
+#echo redis $(package redis)
+#echo setuptools $(package setuptools)
+#exit
 
 # Update the git repo
 # usage:  gitup  git@github.com:user package [revision]
@@ -106,7 +149,12 @@ symlink ()
 testpy ()
 {
     #echo ${prefix}look for $1
-    python -c "import imp; imp.find_module(\"$1\")" 2>/dev/null
+    if test $1 == setuptools; then
+        # setuptools is doing something funky with the path
+        python -c "import setuptools" 2>/dev/null
+    else
+        python -c "import imp; imp.find_module(\"$1\")" 2>/dev/null
+    fi
     return
 }
 
@@ -117,7 +165,7 @@ testpy ()
 # If it does not, it first asks that the user is in a virtual environment.
 # We don't check if it is the _correct_ virtual environment on the assumption
 # that virtual environments are easy to destroy and recreate.
-check_django ()
+check_environment ()
 {
     testpy django >/dev/null 2>&1 && return
     cat <<EOF
@@ -125,19 +173,24 @@ The django environment is not ready.  You should run django sites from a
 virtual environment using only very few system packages.  This allows
 different django applications to coexist on the same server without colliding.
 Either conda or virtualenv environments will work.
-
-Your environment should contain:
-
-    pip numpy scipy h5py matplotlib
-
-Any additional packages will be fetched using pip.
 EOF
-    read -p "${prefix}are you in the django virtual environment? " -n 1 -r
+    ! test $use_conda && cat <<EOF
+
+Your virtual environment should contain:
+
+    $requires
+EOF
+    test $use_conda && installer=conda || installer=pip
+    cat <<EOF
+
+Additional packages will be installed using $installer.
+EOF
+
+    read -p "${prefix}are you in a virtual environment? " -n 1 -r
     echo
-    if [[ ! $REPLAY =~ ^[Yy]$ ]]; then
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
     fi
-    pip install django
 }
 
 
@@ -145,14 +198,18 @@ EOF
 # initialization: install python and initialize the repo directory
 
 #set -x
+
 # start in the repo directory
 cd "$(dirname "$0")"
 
 # Update python environment
-for p in $requires; do ! testpy $p && echo "${prefix}missing $p" && exit 1; done
-check_django
-for p in $installs; do ! testpy $p && echo "${prefix}installing $p" && pip install $p; done
-for p in $djinstalls; do ! testpy $p && echo "${prefix}installing $p" && pip install django-$p; done
+check_environment
+if test -z $use_conda; then
+    for p in $requires; do ! testpy $p && echo "${prefix}missing $p" && exit 1; done
+else
+    for p in $requires; do ! testpy $p && ! $install $(package $p) && exit; done
+fi
+for p in $installs; do ! testpy $p && echo "${prefix}installing $p" && ! $install $(package $p) && exit; done
 
 # Update our own repo
 echo "${prefix}checking repo $(grep url .git/config | sed -e's/^.*://;s/.git$//')"
